d78c77c2c12dfa5c411734188afe7786a0de26e2
Arash Moayyedi <arash.moayedi1@gmail.com>
2019-02-06 00:41:31 +0330

    Fix email confirmation link not working

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -1,10 +1,15 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_auth.registration.views import VerifyEmailView, RegisterView
+from allauth.account.views import confirm_email
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('items/', include('items.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    path('registration/', RegisterView.as_view(), name='account_signup'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$', confirm_email, name='account_confirm_email'),
+    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),
     path('token-auth/', obtain_jwt_token),
 ]
